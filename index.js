d9fcf1616ba6aa6dc31e99f6c74697e297937bdf
Tyrone <tyrone.cloma0219@gmail.com>
2022-07-18 10:29:43 -0400

    solution

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  I used the split() method with a parameter of "" string method to seperate each character of a given string
+  into an array called splitWord. I then itirated over the array and placed each element in a new array called
+  invertedWord, using unshift() to flip the array backwards. I itirated over the invertedWord array and added
+  each element into an empty string variable called newWord. I used an if statement to check if newWord has the
+  exact same value as the original given string
 */
 
 // You can run `node index.js` to view these console logs
